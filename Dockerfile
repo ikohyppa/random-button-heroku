21aa187654831515c054958fc8ce81b7450d616b
ikohyppa <iko.hyppanen@gmail.com>
2021-07-16 19:28:28 +0300

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM ubuntu:18.04
+
+EXPOSE 3000
+
+WORKDIR /usr/src/app
+
+RUN apt-get update
+RUN apt-get install -y curl
+RUN curl -sL https://deb.nodesource.com/setup_14.x | bash
+RUN apt-get install -y nodejs
+
+COPY . .
+
+RUN npm install
+
+RUN npm install -g serve
+
+RUN npm run build
+
+CMD serve -s -l 5000 build
--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [random, setRandom] = useState(0);
+
+  const getRandom = () => {
+    const newRandom = Math.floor(Math.random() * 100);
+    setRandom(newRandom);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button onClick={getRandom}>Press to get random number</button>
+      <h3>{random}</h3>
     </div>
   );
-}
+};
 
 export default App;
